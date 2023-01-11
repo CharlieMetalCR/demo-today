7f29f78d147e9dc57b1f25cb8600349a6370c307
Carlos Blanco <carlosblancobolanos@gmail.com>
2023-01-11 17:32:51 -0600

    All installed in main folder

---
+++ my-app/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Esto es parte de la lección de hoy.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          No quiero hacer un sitio en react
+        </a>
+        
+<h2>Testing</h2>
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
